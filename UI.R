17024a25da714187d69cf5de721a8754267a3a6f
rockerrocker <rockerrocker@users.noreply.github.com>
2014-08-25 00:22:51 +0100

    Update UI.R

--- UI.R
+++ UI.R
@@ -6,15 +6,18 @@ shinyUI(fluidPage(
     # Application title
     titlePanel("BMI Calculator"),
     
-    # Sidebar with a slider input for the number of bins
+    # Sidebar with text inputs for weight and height
     sidebarLayout(
         sidebarPanel(
             h4("Input your details:"),
             numericInput("weight", "Weight, kg:", 0),
-            numericInput("height", "Height, m:", 0)
+            numericInput("height", "Height, m:", 0),
+            h4("BMI Calculation:"),
+            p("= Weight(kg) / Height(m)^2"),
+            img(src="BMI_Eqn.png", height = 72, width = 201)
         ),
         
-        # Show a plot of the generated distribution
+        # Show a result and plot
         mainPanel(
             
             textOutput("text1"),
